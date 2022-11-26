e4cff213690cc04e63de714854a57bf690ea2f43
Maksym Bardakh <46656676+Zu9zwan9@users.noreply.github.com>
2022-11-27 00:20:01 +0200

    Delete src/images directory

--- src/images/images.module.ts
+++
@@ -1,9 +0,0 @@
-import { Logger, Module } from '@nestjs/common';
-import { ImagesService } from './images.service';
-import { ImagesController } from './images.controller';
-
-@Module({
-  providers: [Logger, ImagesService],
-  controllers: [ImagesController],
-})
-export class ImagesModule {}
